674b995ba91509247c20dbf00f197437c9361651
Rodrigo Eche <echerodrigo@yahoo.com>
2022-10-09 11:33:40 -0300

    Append my second JS mini program iniciatico... with notes and screenshots

---
+++ multiplicacion.js
@@ -0,0 +1,18 @@
+// archivo que implementa una funcion simple cuyo resultado se muestra en la terminal
+// para ejecutar desde la terminal> node multiplicacion.js
+// RE 9/Oct/2022 (creado como .js editado con Notepad)
+
+function multiplicar(a,b){
+	return a*b
+}
+
+var x = 3      
+var y = 5
+console.log('El resultado de multiplicar '+ x + ' * ' + y + ' es ' + multiplicar(x,y))
+
+x=2 
+y=-4 
+var producto = x*y
+console.log('El resultado de multiplicar '+ x + ' * ' + y + ' es ' + producto)
+
+console.log('Fin del super programa :-) un pequeño paso inicial...')
