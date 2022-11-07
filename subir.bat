7340b0335cc4cf4ea4e1e1474cf74156c81f52e2
Rodrigo Eche <echerodrigo@yahoo.com>
2022-11-07 18:23:28 -0300

    hola prueba

--- subir.bat
+++ subir.bat
@@ -2,8 +2,8 @@ clear
 git status
 git add .
 git status
-
-git commit -m " Ultimo push "`date +%Y-%B-%d-%T`"  "
+git commit
+git commit -amend " Ultimo push "`date +%Y-%b-%d-%T`"  "
 git status
 git push
 git log
