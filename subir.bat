431b06e8c0cd88bf7a662b14fb4355974c35f992
Rodrigo Eche <echerodrigo@yahoo.com>
2022-11-07 18:24:46 -0300

    nueva prueba

--- subir.bat
+++ subir.bat
@@ -3,7 +3,7 @@ git status
 git add .
 git status
 git commit
-git commit -amend " Ultimo push "`date +%Y-%b-%d-%T`"  "
+git commit --amend " Ultimo push "`date +%Y-%b-%d-%T`"  "
 git status
 git push
 git log
