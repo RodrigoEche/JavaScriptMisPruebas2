7fb2732847dcca76b7b8cb70c71aa8fc0df2f170
Rodrigo Eche <echerodrigo@yahoo.com>
2022-11-07 18:25:45 -0300

    Push: 2022-nov.-07-18:25:45

--- subir.bat
+++ subir.bat
@@ -2,8 +2,8 @@ clear
 git status
 git add .
 git status
-git commit
-git commit --amend " Ultimo push "`date +%Y-%b-%d-%T`"  "
+
+git commit -m " Push: "`date +%Y-%b-%d-%T`"  "
 git status
 git push
 git log
