3431202f1c4e0509c24c02d0678be85654fda926
RodrigoEche <echerodrigo@yahoo.com>
2022-12-08 13:49:20 -0300

    Push: 2022-diciembre-08-13:49:19

--- MiBaseDatosIndexada/codigo.js
+++ MiBaseDatosIndexada/codigo.js
@@ -4,15 +4,25 @@
 
 var cajadatos, bd;
 
-//function iniciar() {
+function iniciar() {
 	cajadatos = document.getElementById("cajadatos");
 	
 	var boton =  document.getElementById("grabar");
 	boton.addEventListener("click",() => console.log("Hola, en proceso de construcción. Disculpe las molestias.")   );
 	
+	//.open() abre o intenta abrir la BD y devuelve el objeto "solicitud" que desencadena uno de los tres eventos posibles "error" "succes" u "upgradeneeded"
+	//el evento upgradeneeded se llama si no existe aúnla base de datos 
 	var solicitud = indexedDB.open(basededatos);
 	solicitud.addEventListener("error",mostrarError);
 	solicitud.addEventListener("succes",comenzar);
 	solicitud.addEventListener("upgradeneeded",crearbd);
+}
 
-//}
+
+function mostrarError(evento){    alert(" Ocurrió un error de código: " + evento.code + " " + evento.message);    }
+
+function comenzar(evento){ 
+	// se captura de una referencia a la base de datos mediante la prop.
+	// .result y se almacena en la variable global bd para uso posterior 
+	bd = evento.target.result;
+}
--- simula5.js
+++ simula5.js
@@ -5,7 +5,7 @@
   No comentar la funcion 
 */
 function pluck(array, propiedad) {
-    // La función llamada 'pluck' recibe como argumento un array de objetos llamado 'array' y el nombre de una
+    // La función llamada 'pluck' (extraer) recibe como argumento un array de objetos llamado 'array' y el nombre de una
     // propiedad.
     // La función debe devolver un nuevo arreglo con solo los valores dentro de la propiedad recibida
     // Ej:    // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
@@ -23,7 +23,8 @@ function pluck(array, propiedad) {
   }
   /////////////////////////////////////////////////////////////////////////////////////////////////////
   var arrayObjetos = [ { name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 } ];
-
+  console.log(pluck ) ;
+  
   console.log(pluck(arrayObjetos, 'name')) ;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
