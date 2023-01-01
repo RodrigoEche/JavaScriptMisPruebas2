960b206cc3951bda7a97f72fbbfea8ff9776b3d3
RodrigoEche <echerodrigo@yahoo.com>
2023-01-01 17:44:05 -0300

    Push: 2023-January-01-17:44:05

--- rellenaArrayUsandoExtremos.js
+++ rellenaArrayUsandoExtremos.js
@@ -1,19 +1,16 @@
 function rellenaArrayUsandoExtremos(array) {
     // array = [2,6] ----> rellenaArrayUsandoExtremos(array) ----> arraySalida = [2,3,4,5,6]
 
-    //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
     
-    const suma = array.reduce(  (acumulador, actual) => acumulador + actual );  
-    //const suma = array.reduce(  function(a, b) { return a + b; } );  
-
-    //const inicial = 0;
-    //const suma = array.reduce(  (acumulador, actual) => acumulador + actual,   inicial);  
-    //const suma = array.reduce(  function(a, b) { return a + b; }, inicial);  
-    arraySalida = array;
+    
+    const arraySalida = [];
+    for ( i = array[0]; i <= array[1]; i++ )    arraySalida.push(i);
+    
     return arraySalida;
   }   //Fin de funcion ()
 
-    const array = [2, 6];
+    const array = [-2, 3];
     console.log("\nArray de entrada: ");
     console.log(array);
     console.log("\nArray de salida: ");
