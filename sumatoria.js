523cba917d1aef497ae3100477dc14b1dada8be9
RodrigoEche <echerodrigo@yahoo.com>
2023-01-01 16:29:43 -0300

    Push: 2023-January-01-16:29:42

---
+++ sumatoria.js
@@ -0,0 +1,19 @@
+function sumatoria(array) {
+    // array = [2,1,4,6] ----> sumatoria(array) ----> 2 + 1 + 4 + 6 = 13
+
+    //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    
+    const suma = array.reduce(  (acumulador, actual) => acumulador + actual );  
+    //const suma = array.reduce(  function(a, b) { return a + b; } );  
+
+    //const inicial = 0;
+    //const suma = array.reduce(  (acumulador, actual) => acumulador + actual,   inicial);  
+    //const suma = array.reduce(  function(a, b) { return a + b; }, inicial);  
+
+    return suma;
+  }   //Fin de funcion sumatoria()
+
+    const array = [2, 1, -5, 7];
+    console.log(array);
+    console.log("Sumatoria: " + sumatoria(array));
+
