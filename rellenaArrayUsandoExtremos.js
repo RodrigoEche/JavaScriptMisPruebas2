a0ab7c1d6c646c26ee2df56051a5949c027bd3c2
RodrigoEche <echerodrigo@yahoo.com>
2023-01-01 20:27:06 -0300

    Push: 2023-January-01-20:27:04

--- rellenaArrayUsandoExtremos.js
+++ rellenaArrayUsandoExtremos.js
@@ -1,19 +1,23 @@
 function rellenaArrayUsandoExtremos(array) {
     // array = [2,6] ----> rellenaArrayUsandoExtremos(array) ----> arraySalida = [2,3,4,5,6]
-
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push      
-    const arraySalida = [];
-    for ( i = array[0]; i <= array[1]; i++ )    arraySalida.push(i);
-    
-    return arraySalida;
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#examples
+    //https://lineadecodigo.com/javascript/rellenar-un-array-con-numeros-en-javascript/
+    /* const arraySalida = [];
+    for ( i = array[0]; i <= array[1]; i++ )    arraySalida.push(i);    
+    return arraySalida; */
+    const cantidadElementos     =   1 + array[1] - array[0];
+    const arraySalida           =  new Array(cantidadElementos);
+    arraySalida.fill(0);
+    arraySalida.forEach(    (valor,indice)   =>   arraySalida[indice] = indice + array[0]  );
+    return arraySalida; 
   }   //Fin de funcion ()
-
-  /*
-    const array = [-2, 3];
+    
+    const array = [-1, 4];
     console.log("\nArray de entrada: ");
     console.log(array);
     console.log("\nArray de salida: ");
-    console.log( rellenaArrayUsandoExtremos(array)); */
+    console.log( rellenaArrayUsandoExtremos(array)); 
 
     module.exports = rellenaArrayUsandoExtremos;
 
