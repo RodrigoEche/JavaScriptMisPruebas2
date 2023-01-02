0fb3adc63ce0c163b7340b74eceb4527cc20c0e7
RodrigoEche <echerodrigo@yahoo.com>
2023-01-02 08:39:32 -0300

    Push: 2023-January-02-08:39:32

---
+++ freeCodeCamp.org mis soluciones/construye-objetos-en-javascript.txt.js
@@ -0,0 +1,10 @@
+const myDog = {
+  // Cambia solo el código debajo de esta línea
+
+  name: "Pancha",
+  legs: 4,
+  tails: 1,
+  friends: ["Amada", "Jano"]
+
+  // Cambia solo el código encima de esta línea
+};
