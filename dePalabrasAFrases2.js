4f9bba02691591fb38b8d048e3881e475365d722
RodrigoEche <echerodrigo@yahoo.com>
2023-01-19 07:03:03 -0300

    Push: 2023-January-19-07:03:00

--- dePalabrasAFrases2.js
+++ dePalabrasAFrases2.js
@@ -4,7 +4,7 @@ function dePalabrasAFrase(palabras) {
     // con espacios entre cada palabra
     // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
     // Tu código:
-
+    
     return palabras.join(" ");
   }
   
